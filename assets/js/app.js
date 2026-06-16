f68bb541b16aa05ea16f4b2d5cb8b777986ec0f9
Eslam Eshikh <moqawel1215@gmail.com>
2026-06-16 21:21:19 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
-// app
+window.addEventListener('scroll', () => {
+
+const header = document.querySelector('.header');
+
+if(window.scrollY > 50){
+
+header.style.background = 'rgba(255,255,255,.12)';
+header.style.backdropFilter = 'blur(20px)';
+
+}else{
+
+header.style.background = 'rgba(255,255,255,.35)';
+header.style.backdropFilter = 'blur(12px)';
+
+}
+
+});
